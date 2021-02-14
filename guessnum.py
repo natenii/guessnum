d7f8a925ab30a0dc189c9f0b1434271936ae7603
Nate Ni <natengai.nn@gmail.com>
2021-02-14 18:51:17 +0800

    Added user define range

--- guessnum.py
+++ guessnum.py
@@ -1,11 +1,12 @@
 import random
+start = int(input('請輸入起始範圍： '))
+end = int(input('請輸入結束範圍： '))
 
-r = random.randint(1,100)
+r = random.randint(start, end)
 count = 0
 while True:
     count += 1
-    num = input('猜數字：')
-    num = int(num)
+    num = int(input('猜數字：'))
     if num == r:
         print('猜對了！')
         print('這是猜了第', count, '次')
